764ac97e1ff9c234e31dd83c16578b28f5868c1f
sly-agustin <agustin_sly@hotmail.com>
2023-04-01 15:36:40 -0300

    feat; added login middleware to mod route

--- src/deliveries/express/routes/mod.route.js
+++ src/deliveries/express/routes/mod.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import modController from '../../../controllers/mod/modController';
+import { authJwt } from "../middlewares";
 import { verifyMod } from '../middlewares'
 
 const router = express.Router();
 
 // Validators pending
 router.get('/:id', modController.getMod);
-router.post('/',[verifyMod.checkModNameDuplicate], modController.postMod);
+router.post('/', authJwt.verifyToken, [verifyMod.checkModNameDuplicate], modController.postMod);
 
 export default router
