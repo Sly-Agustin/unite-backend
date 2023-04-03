a955eb842d146b212eb655dc8406771e56a2fa86
sly-agustin <agustin_sly@hotmail.com>
2023-04-03 16:40:53 -0300

    refactor: added validators to routes

--- src/deliveries/express/routes/auth.route.js
+++ src/deliveries/express/routes/auth.route.js
@@ -1,18 +1,20 @@
 import express from 'express';
 import { verifySignUp } from '../middlewares';
-import authController from '../../../controllers/authController'
+import authController from '../../../controllers/authController';
+import Validator from '../middlewares/Validator';
 
 const router = express.Router();
 
 router.post(
   '/register', 
+  Validator('registerSchema'),
   [
     verifySignUp.checkDuplicateUsernameOrEmail,
     verifySignUp.checkRolesExisted,
   ],
   authController.register
 )
-router.post('/signin', authController.signIn)
+router.post('/signin', Validator('signInSchema'), authController.signIn)
 router.post('/signout', authController.signOut)
 
 export default router
--- src/deliveries/express/routes/game.route.js
+++ src/deliveries/express/routes/game.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import gameController from '../../../controllers/game/gameController';
 import { authJwt } from "../middlewares";
+import Validator from '../middlewares/Validator';
 
 const router = express.Router();
 
 // Validators pending
 router.get('/all', gameController.getAllGames);
-router.post('/new', authJwt.verifyToken, gameController.postGame);
+router.post('/new', Validator('newGameSchema'), authJwt.verifyToken, gameController.postGame);
 
 export default router
