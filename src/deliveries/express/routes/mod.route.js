309eb3645fa83eec86bd09d3609e45990da62d5d
sly-agustin <agustin_sly@hotmail.com>
2023-04-03 15:05:22 -0300

    refactor: middlewares in mod routes

--- src/deliveries/express/routes/mod.route.js
+++ src/deliveries/express/routes/mod.route.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import modController from '../../../controllers/mod/modController';
 import { authJwt } from "../middlewares";
-import { verifyMod } from '../middlewares'
+import { verifyMod } from '../middlewares';
+import { verifyModOwner } from '../middlewares';
 
 const router = express.Router();
 
 // Validators pending
-router.get('/:id', modController.getMod);
-router.post('/', authJwt.verifyToken, [verifyMod.checkModNameDuplicate], modController.postMod);
+router.get('/:id', verifyMod.checkModExists, modController.getMod);
+router.post('/', authJwt.verifyToken, verifyMod.checkModNameDuplicate, modController.postMod);
+router.put('/:id', authJwt.verifyToken, verifyMod.checkModExists, verifyModOwner.checkOwnerSameAsActiveUser, modController.putMod);
 
 export default router
